86c2551fed4cb19ef9a12ba0a9b0f1e79967ed6a
tracy <keeyatracy@gmail.com>
2025-11-19 13:24:50 +0300

    my second commit on strings

--- strings.py
+++ strings.py
@@ -6,3 +6,19 @@ print(word[3:7])
 # 3. Reverse the string "Python" using slicing.
 name = "Python"
 print(name[::-1])
+
+
+# String Concatenation
+# Strings can be combined using the + operator:
+
+# Practice Questions
+# 1. Concatenate "Python" and "Rocks" with a space in between.
+Language = "Python"
+Thing = "Rocks"
+Concatenation = Language + " " + Thing
+print(Concatenation)
+# 2. Combine "Data" and "Science" into a single string.
+Info = "Data"
+Subject= "Science"
+Combined = Info + Subject
+print(Combined)
